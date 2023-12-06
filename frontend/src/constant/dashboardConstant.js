af01e86fbdf07cead4ed42e359bb1ddc3342b62d
yanshiuh <yanshiuh12372@gmail.com>
2023-12-06 10:54:04 +0800

    Add dashboard constant

---
+++ frontend/src/constant/dashboardConstant.js
@@ -0,0 +1,39 @@
+import { PiStudentFill } from "react-icons/pi";
+import { FaChalkboardTeacher } from "react-icons/fa";
+import { BsPersonFillLock } from "react-icons/bs";
+import { SiGoogleclassroom } from "react-icons/si";
+import { FaFileSignature } from "react-icons/fa6";
+import { RiMoneyDollarCircleFill } from "react-icons/ri";
+
+export const items = [
+  {
+    name: "Student",
+    icon: <PiStudentFill size={60} className="m-auto" />,
+    path: "/students",
+  },
+  {
+    name: "Teacher",
+    icon: <FaChalkboardTeacher size={60} className="m-auto" />,
+    path: "",
+  },
+  {
+    name: "Admin",
+    icon: <BsPersonFillLock size={55} className="m-auto" />,
+    path: "",
+  },
+  {
+    name: "Class",
+    icon: <SiGoogleclassroom size={60} className="m-auto" />,
+    path: "",
+  },
+  {
+    name: "Attendance",
+    icon: <FaFileSignature size={50} className="m-auto" />,
+    path: "",
+  },
+  {
+    name: "Salary",
+    icon: <RiMoneyDollarCircleFill size={60} className="m-auto" />,
+    path: "",
+  },
+];
